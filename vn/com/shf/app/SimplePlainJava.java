dd7918ad079e6dd65d0dd1c4f64a5cfc29f4f214
Lê Hoàng Bảo Lâm <>
2024-08-18 10:01:11 +0700

    add main

---
+++ vn/com/shf/app/SimplePlainJava.java
@@ -0,0 +1,10 @@
+package vn.com.shf.app;
+import java.util.List;
+/**
+ * SimplePlainJava
+ */
+public class SimplePlainJava {
+    public static void main(String[] args) {
+        System.out.println("List of args " + List.of(args));
+    }
+}
